9166fc131753f1a15a883e38faea8d4648c7fec0
“TienDung1706“ <tiendung17062k4@gmail.com>
2025-03-17 15:18:08 +0700

    update profile 2

--- app/src/main/java/vn/tiendung/socialnetwork/Fragment/ProfileFragment.java
+++ app/src/main/java/vn/tiendung/socialnetwork/Fragment/ProfileFragment.java
@@ -8,14 +8,37 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import vn.tiendung.socialnetwork.Adapter.ViewPagerAdapter;
 import vn.tiendung.socialnetwork.R;
 
 public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        // Tạo View trước khi return
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = view.findViewById(R.id.viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity());
+        viewPager.setAdapter(adapter);
+
+        // Liên kết TabLayout với ViewPager2
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            if (position == 0) {
+                tab.setText("Bài viết gần đây");
+            } else {
+                tab.setText("Album ảnh");
+            }
+        }).attach();
+
+        return view; // ĐẢM BẢO return SAU KHI setup xong
     }
 }
 
